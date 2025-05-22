2cb9c2fbf29dbc23797fc3f6a69021a92d97b52b
shreyas <shreyas.vangala1@gmail.com>
2025-05-22 20:16:29 +0530

    removed navBar

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -33,7 +33,7 @@ const Index = () => {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+      {/* <Navbar /> */}
       <main>
         <Hero />
         <Benefits />
